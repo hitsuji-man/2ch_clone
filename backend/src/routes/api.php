3ecf2ac4b084c6a1778de3944969ed0449a14c76
hitsuji-man <renton1q84@gmail.com>
2025-10-29 18:25:16 +0900

    Threadモデルの作成とルーティングの設定

--- backend/src/routes/api.php
+++ backend/src/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\ThreadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/threads', [ThreadController::class, 'index']);
+Route::post('/threads', [ThreadController::class, 'store']);
+Route::get('/threads/{id}', [ThreadController::class, 'show']);
