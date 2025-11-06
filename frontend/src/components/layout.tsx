d3ab77197c893ac2828954ec9976d89087aa318b
hitsuji-man <renton1q84@gmail.com>
2025-11-06 14:07:39 +0900

    共通レイアウトの作成

---
+++ frontend/src/components/layout.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+
+type LayoutProps = {
+  children: ReactNode;
+  title?: string;
+  bg?: string;
+};
+
+const Layout = ({
+  children,
+  title,
+  bg = "bg-orange-300 bg-opacity-50",
+}: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className={`min-h-screen ${bg}`}>
+        <header>
+          <h1 className="pt-6 text-4xl font-bold text-center">掲示板</h1>
+        </header>
+        <main className="container mx-auto p-4">{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
